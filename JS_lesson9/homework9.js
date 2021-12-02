999c7fe20a36f624a0f5f47781267b74ee6d9a0e
vkopka <vkopka@gmail.com>
2021-12-02 11:56:17 +0200

    JS Lesson 9 Commit 1

---
+++ JS_lesson9/homework9.js
@@ -0,0 +1,75 @@
+// Все робити за допомоги js.
+// - створити блок,
+//     - додати йому класи wrap, collapse, alpha, beta
+// - додати стилі(довільні значення) : колір фону, колір тексту, розмір тексту
+// - додати цей блок в body.
+// - клонувати його повністю, та додати клон в body.
+const template = document.createElement('div');
+template.innerText = 'This block';
+template.classList.add('wrap', 'collapse', 'alpha', 'beta');
+template.style.background = 'gray';
+template.style.color = 'blue';
+template.style.fontSize = '24px';
+document.body.appendChild(template);
+const divClone = template.cloneNode(true);
+divClone.innerText = 'Clone block';
+document.body.appendChild(divClone);
+
+// - Є масив:
+//     ['Main','Products','About us','Contacts']
+// Взяти файл template1.html та додати в нього сценарій, котрий для кожного елементу масиву
+// створює li та додає його до блоку .menu
+// Завдання робити через цикли.
+let className = 'menu';
+const elementMenu = document.getElementsByClassName(className);
+const menuItems = ['Main', 'Products', 'About us', 'Contacts'];
+for (const menuItem of menuItems) {
+    const itemLi = document.createElement('li');
+    itemLi.innerText = menuItem;
+    elementMenu[0].appendChild(itemLi);
+}
+
+// - Є масив
+let coursesAndDurationArray = [
+    {title: 'JavaScript Complex', monthDuration: 5},
+    {title: 'Java Complex', monthDuration: 6},
+    {title: 'Python Complex', monthDuration: 6},
+    {title: 'QA Complex', monthDuration: 4},
+    {title: 'FullStack', monthDuration: 7},
+    {title: 'Frontend', monthDuration: 4}
+];
+// Для кожного елементу масиву зробити блок в якому вивести інформацію про title та monthDuration
+// Завдання робити через цикли.
+for (const course of coursesAndDurationArray) {
+    let text = '';
+    for (const courseKey in course) {
+        text += `${courseKey}: ${course[courseKey]} `;
+    }
+    const template = document.createElement('div');
+    template.innerText = text;
+    document.body.appendChild(template);
+}
+
+// - Є масив coursesAndDurationArray
+// За допомоги сценарію для кожного елементу масиву зробити <div class='item'> ,
+// в якому буде <h1 class='heading'>  з title  елементу, та <p class='description'> з monthDuration елементу.
+//     Завдання робити через цикли.
+for (const course of coursesAndDurationArray) {
+    const template = document.createElement('div');
+    template.classList.add('item');
+    document.body.appendChild(template);
+    className = 'item';
+    const elementDiv = document.getElementsByClassName(className);
+    for (const courseKey in course) {
+        let element = {};
+        if (courseKey === 'title') {
+            element = document.createElement('h1');
+            element.classList.add('heading');
+        } else if (courseKey === 'monthDuration') {
+            element = document.createElement('p');
+            element.classList.add('description');
+        }
+        element.innerText = `${courseKey}: ${course[courseKey]}`;
+        elementDiv[elementDiv.length - 1].appendChild(element);
+    }
+}
